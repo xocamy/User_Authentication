0191d40fb5c6bc416d9d82ebe9d40ec863e52254
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-04 19:05:52 +0530

    modified register.js to take specific input

--- register.js
+++ register.js
@@ -1,7 +1,13 @@
 
 const formSubmit = () => {
-    var username = document.getElementById('username');
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    alert(`Your Username is : ${username.value} and password is : ${password.value}` )
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    if( username === "" || email === "" || password === "")
+    {
+        alert("Please fill the required info!")
+    }else{
+        alert(`Your Username is : ${username} and password is : ${password}` )
+    }
 }
