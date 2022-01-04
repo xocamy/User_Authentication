2c766d4da0cb4b59ae45e229825801e7605b9220
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-04 17:54:33 +0530

    added login.js file to add interactivity to the login page

---
+++ login.js
@@ -0,0 +1,6 @@
+
+const submitForm = () => {
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    alert(`Your Username is : ${username.value} and password is : ${password.value}` )
+}
