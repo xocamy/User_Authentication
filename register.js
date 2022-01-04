f4c3d06d47a82c142ec2fb7d29b4ec7ece65460b
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-04 18:08:21 +0530

    updated register.js to get user's info

---
+++ register.js
@@ -0,0 +1,7 @@
+
+const formSubmit = () => {
+    var username = document.getElementById('username');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    alert(`Your Username is : ${username.value} and password is : ${password.value}` )
+}
