6fd94ca56c90ac40c3d27a94fdac129b0f4d3d82
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-08 21:22:23 +0530

    updated register request handle api to store hashed password

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const user = require('../Models/user');
 const router = express.Router();
+const bcrypt = require('bcrypt')
 
 // login handle
 router.get('/login' , (req ,res) => {
@@ -29,13 +30,24 @@ router.post('/login' , async (req ,res) => {
 router.post('/register' , async ( req , res) => {
     try{
        const {username,email, password} = req.body;
-        const newUser = new user ({
-            username  : username,
-            email : email,
-            password : password
+        const saltRounds = 10;
+       // Technique 1 (generate a salt and hash on separate function calls):
+       const newUser = new user ({
+           username  : username,
+           email : email,
+           password : password
         })
-        const User =  newUser.save();
-       res.redirect('/login');
+        bcrypt.genSalt(10 , (err , salt) => 
+        bcrypt.hash(newUser.password , salt , (err , hash) => {
+            if( err ) throw err;
+            newUser.password = hash;
+            newUser.save().then((value) => {
+                console.log(value)
+                res.redirect('/login');
+            }).catch( value => console.log( value));
+        }));
+        console.log(newUser.password);
+        //  const User =  newUser.save();
     } catch(err) {
         res.status(500).json(err)
     }
