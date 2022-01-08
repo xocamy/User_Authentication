6ef0301a60967895435623c90ea6a8338e2d4cf5
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-08 11:44:52 +0530

    handled register request in user.js route and redirected to login page

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ const user = require('../Models/user');
 const router = express.Router();
 
 // login handle
+router.get('/login' , (req ,res) => {
+    res.render('login');
+}) 
 router.post('/login' , (req ,res) => {
     const { username , password} = req.body;
 })
@@ -11,14 +14,14 @@ router.post('/login' , (req ,res) => {
 // register handle
 router.post('/register' , async ( req , res) => {
     try{
-        const {username,email, password} = req.body;
+       const {username,email, password} = req.body;
         const newUser = new user ({
             username  : username,
             email : email,
             password : password
         })
-        const User = await newUser.save();
-        res.redirect('users/login');
+        const User =  newUser.save();
+       res.redirect('/login');
     } catch(err) {
         res.status(500).json(err)
     }
